43aca0fdc63dc73380ecba2ed006188f46f5a347
Scott Firestone <scott.firestone.dev@gmail.com>
2016-02-02 17:47:44 -0700

    has delete and edit routing and views

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -16,14 +17,38 @@ class TaskManagerApp < Sinatra::Base
     erb :new
   end
 
-  post '/tasks' do
+  get '/tasks/:id' do |id|
+    @task = task_manager.find(id.to_i)
+    erb :show
+  end
+
+  put '/tasks/:id' do |id|
+    task_manager.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  get '/tasks/:id/edit' do |id|
+    @task = task_manager.find(id.to_i)
+    erb :edit
+  end
+
+  post "/tasks/:id" do |id|
+    @task = task_manager.update(params[:task], id)
+    redirect "/tasks"
+  end
+
+  post "/tasks" do
     task_manager.create(params[:task])
     redirect '/tasks'
   end
 
-  get '/tasks/:id' do |id|
-    @task = task_manager.find(id.to_i)
-    erb :show
+  delete "/tasks/:id" do |id|
+    task_manager.delete(id.to_i)
+    redirect "/tasks"
+  end
+
+  error do
+    "404: that path does not exist"
   end
 
   def task_manager
